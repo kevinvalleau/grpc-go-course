608c1415968117e47f7bfa61e083e14aa5f57744
kevinvalleau <62773562+kevinvalleau@users.noreply.github.com>
2022-05-07 15:10:16 -0400

    Update for tls

--- greet/client/main.go
+++ greet/client/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"log"
-	"time"
+	//"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 
 	pb "github.com/kevinvalleau/grpc-go-course/greet/proto"
 )
@@ -13,7 +13,20 @@ import (
 var addr string = "localhost:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tls := true
+	opts := []grpc.DialOption{}
+
+	if tls {
+		certFile := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(certFile,"")
+
+		if err != nil {
+			log.Fatalf("Error while loading CA trust certificates: %v\n", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	}
+	conn, err := grpc.Dial(addr, opts...)
 
 	if err != nil {
 		log.Fatalf("Failed to connect: %v\n", err)
@@ -23,10 +36,10 @@ func main() {
 
 	c := pb.NewGreetServiceClient(conn)
 
-	//doGreet(c)
+	doGreet(c)
 	//doGreetManyTimes(c)
 	//doLongGreet(c)
 	//doGreetEveryone(c)
-	doGreetWithDeadline(c, 5*time.Second)
+	// doGreetWithDeadline(c, 5*time.Second)
 	// doGreetWithDeadline(c, 1*time.Second)
 }
